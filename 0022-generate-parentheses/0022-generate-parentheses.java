cd68e4d9d1c298c9c0b41a6c15e3e40681518ab1
Wrath_Of_God <ht203556@gmail.com>
2026-08-28 21:23:08 +0530

    Time: 1 ms (85.86%), Space: 44 MB (96.55%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+
+        StringBuilder tmp = new StringBuilder();
+
+        fun(0, 0, n, tmp, res);
+
+        return res;
+    }
+    static void fun(int open, int close,int n, StringBuilder tmp,List<String> res){
+        if(open == n && close == n){
+            res.add(tmp.toString());
+            return;
+        }
+
+        if(open < n){
+            tmp.append('(');
+
+            fun(open + 1, close, n, tmp, res);
+
+            tmp.deleteCharAt(tmp.length() - 1);
+                                   
+        }
+         // close bracket
+        if (close < open) {
+
+            tmp.append(')');
+
+            fun(open, close + 1, n, tmp, res);
+
+            tmp.deleteCharAt(tmp.length() - 1);
+        }
+    }
+}
