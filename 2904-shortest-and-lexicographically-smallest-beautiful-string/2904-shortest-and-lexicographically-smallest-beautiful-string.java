9b803ed583c8ccc9ebaf215f61800b0c11fa8eb3
Wrath_Of_God <ht203556@gmail.com>
2026-08-30 17:15:58 +0530

    Time: 2 ms (56.7%), Space: 44 MB (66.77%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -2,32 +2,41 @@ class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
         int left = 0;
         int count = 0;
+
         String ans = "";
 
-        for(int right = 0;right < s.length();right++){
-            if(s.charAt(right) == '1'){
+        for (int right = 0; right < s.length(); right++) {
+
+            if (s.charAt(right) == '1') {
                 count++;
             }
 
-            while(count > k){
-                if(s.charAt(right) == '1'){
+            // We have exactly k ones
+            while (count > k) {
+                if (s.charAt(left) == '1') {
                     count--;
                 }
                 left++;
             }
 
-            while(left < right && s.charAt(left) == '0'){
+            // Try to remove unnecessary zeros from the left
+            while (left < right && s.charAt(left) == '0') {
                 left++;
             }
 
-            if(count == k){
-                String current = s.substring(left,right + 1);
+            // Exactly k ones
+            if (count == k) {
+                String current = s.substring(left, right + 1);
 
-                if(ans.equals("") || current.length() < ans.length() || (current.length() == ans.length() && current.compareTo(ans) < 0)){
+                if (ans.equals("")
+                        || current.length() < ans.length()
+                        || (current.length() == ans.length()
+                            && current.compareTo(ans) < 0)) {
                     ans = current;
                 }
             }
         }
+
         return ans;
     }
 }
