7cb81eeed1f0974abcea4fa6cb72cc353dac666a
Himanshu_Thakur <ht203556@gmail.com>
2026-09-07 19:07:18 +0530

    Time: 14 ms (26.62%), Space: 46.4 MB (42%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        HashMap<Character,Integer> map = new HashMap<>();
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        for(char ch : s.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0) + 1);
+        }
+
+        for(char ch : t.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0) - 1);
+        }
+
+        for(int count : map.values()){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
