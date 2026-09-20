04228877a0164e2dfc1d9e2e2cc4f1b3517814bd
Himanshu_Thakur <ht203556@gmail.com>
2026-09-20 20:17:49 +0530

    Time: 14 ms (49.64%), Space: 42.1 MB (54.77%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+         int low = 1;
+        int high = n;
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (isBadVersion(mid)) {
+                // mid is bad, but it could be the first bad version
+                high = mid;
+            } else {
+                // mid is good, so first bad must be after mid
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
